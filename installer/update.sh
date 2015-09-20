a4018fe4a3bbecb8c37500925de40496406f9e05
koenschepens <kschepens@gmail.com>
2015-09-20 20:29:51 +0200

    asdf

--- installer/update.sh
+++ installer/update.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+now=$(date +"%m_%d_%Y_%T")
+echo "********* update.sh **********" >> /var/log/keypadUpdater.log
+echo $now >> /var/log/keypadUpdater.log
+echo $USER >> /var/log/keypadUpdater.log
+
 cd /usr/bin/oldphone/OldPhone-Keypad
 
-git update-server-info
 git pull 
 
 cd /usr/bin/oldphone/OldPhone-Keypad/installer
