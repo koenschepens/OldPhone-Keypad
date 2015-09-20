f3e7002f474afc483ce3b44e46dc6cb116b2eb46
koenschepens <kschepens@gmail.com>
2015-09-20 21:12:54 +0200

    d

--- installer/update.sh
+++ installer/update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-now=$(date +"%m_%d_%Y_%T")
-echo "********* update.sh **********" >> ~/keypadUpdater2.log
+#now=$(date +"%m_%d_%Y_%T")
+#echo "********* update.sh **********" >> ~/keypadUpdater2.log
 
-exec (cd /usr/bin/oldphone/OldPhone-Keypad; git pull; cd /usr/bin/oldphone/OldPhone-Keypad/installer; ./install.sh) >> ~/keypadUpdater2.log
+(cd /usr/bin/oldphone/OldPhone-Keypad; git pull; cd /usr/bin/oldphone/OldPhone-Keypad/installer; ./install.sh) >> ~/keypadUpdater2.log
