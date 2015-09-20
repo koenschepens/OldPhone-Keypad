6d394c483d392d060520004fb9b42219b7bada7d
koenschepens <kschepens@gmail.com>
2015-09-20 15:51:52 +0200

    Installer fixes

--- installer/install.sh
+++ installer/install.sh
@@ -1,1 +1,21 @@
-#/bin/sh
+#!/bin/bash
+
+clear
+
+echo "installing..."
+
+echo "disabling oldkeypad services"
+/etc/init.d/oldkeypad stop
+
+echo "deleting existing oldkeypad files"
+rm /etc/init.d/oldkeypad
+
+echo "copying new files"
+mkdir /usr/share/oldkeypad
+cp ./kbdout.config /usr/share/oldkeypad/kbdout.config
+
+cp ./keypadd.sh /etc/init.d/keypadd
+chmod 777 /etc/init.d/keypadd
+
+echo "enabling oldkeypad service"
+/etc/init.d/oldkeypad start
---
+++ installer/keypadd.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# path to xinit exec
+DAEMON=/usr/bin/python
+
+# startup args
+DAEMON_OPTS=" /usr/bin/oldphone/OldPhone-Keypad/program/kbdout.py"
+
+# script name
+NAME=oldphonekeypad
+
+# app name
+DESC=OldPhoneKeyPad
+
+# user
+RUN_AS=root
+
+# Path of the PID file
+PID_FILE=/var/run/kbdout.pid
+
+############### END EDIT ME ##################
+
+test -x $DAEMON || exit 0
+
+set -e
+
+case "$1" in
+  start)
+        echo "Starting $DESC"
+        start-stop-daemon --start -c $RUN_AS --background --pidfile $PID_FILE  --make-pidfile --exec $DAEMON -- $DAEMON_OPTS
+        ;;
+  stop)
+        echo "Stopping $DESC"
+        start-stop-daemon --stop --pidfile $PID_FILE
+        ;;
+
+  restart|force-reload)
+        echo "Restarting $DESC"
+        start-stop-daemon --stop --pidfile $PID_FILE
+        sleep 5
+        start-stop-daemon --start -c $RUN_AS --background --pidfile $PID_FILE  --make-pidfile --exec $DAEMON -- $DAEMON_OPTS
+        ;;
+  *)
+        N=/etc/init.d/$NAME
+        echo "Usage: $N {start|stop|restart|force-reload}" >&2
+        exit 1
+        ;;
+esac
+
+exit 0
