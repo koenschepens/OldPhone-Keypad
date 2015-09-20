6539dff42eafd18a39c9b47406e170f466361318
koenschepens <kschepens@gmail.com>
2015-09-20 20:21:38 +0200

    d

--- installer/update.sh
+++ installer/update.sh
@@ -2,8 +2,8 @@
 
 cd /usr/bin/oldphone/OldPhone-Keypad
 
-git update-server-info >> /var/log/keypadUpdater.log
-git pull >> /var/log/keypadUpdater.log
+git update-server-info
+git pull 
 
 cd /usr/bin/oldphone/OldPhone-Keypad/installer
 ./install.sh
