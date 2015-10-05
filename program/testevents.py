02c554121a2473fb24ac0d45f3cb790a17896c9f
koenschepens <kschepens@gmail.com>
2015-10-05 21:48:47 +0200

    d

--- program/testevents.py
+++ program/testevents.py
@@ -82,7 +82,7 @@ for option in gpiokeymappings:
         print("IN: " + str(row))
         rows[row] = [column]
         GPIO.setup(row, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(row, GPIO.ANY, callback=row_changed) 
+        GPIO.add_event_detect(row, GPIO.RISING, callback=row_changed) 
     else:
         rows[row].append(column)
 
