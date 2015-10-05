42568d18e79035179eca8ac300a86ac4ca93ca58
koenschepens <kschepens@gmail.com>
2015-10-06 01:34:32 +0200

    asdf

--- program/runner.py
+++ program/runner.py
@@ -1,2 +1,9 @@
 import os
+import logging
+addonFolder = "/home/osmc/.kodi/addons/service.keypad/" 
+
+logging.basicConfig(filename=addonFolder + 'keypad.log',level=logging.INFO)
+
+logging.info("starting keypad")
+
 os.system('sudo python keypad.py')
