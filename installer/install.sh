4022531ee122b0c1a11ce284a15662915aef92da
koenschepens <kschepens@gmail.com>
2015-10-06 00:22:26 +0200

    asdf

--- installer/install.sh
+++ installer/install.sh
@@ -14,7 +14,7 @@ fi
 echo "copying program files"
 cp ../program/* $serviceFolder/
 
-if [ ! -f $serviceFolder/keypad.config ] || [ $1 -eq "f" ]
+if [ ! -f $serviceFolder/keypad.config ] || [ "$1" == "f" ]
 	then
 		echo "copying config"
 		cp ./keypad.config $serviceFolder/keypad.config
