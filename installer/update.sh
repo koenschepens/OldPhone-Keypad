b933d8711640fc1963c9553d0fd117a2b3fdee12
koenschepens <kschepens@gmail.com>
2015-10-06 02:15:11 +0200

    asdf

--- installer/update.sh
+++ installer/update.sh
@@ -3,4 +3,7 @@
 #now=$(date +"%m_%d_%Y_%T")
 #echo "********* update.sh **********" >> ~/keypadUpdater2.log
 
-(cd /usr/bin/oldphone/OldPhone-Keypad; git pull; cd /usr/bin/oldphone/OldPhone-Keypad/installer; ./install.sh) >> ~/keypadUpdater2.log
+sudo service mediacenter stop
+sudo git pull
+sudo ./install.sh
+sudo service mediacenter start
