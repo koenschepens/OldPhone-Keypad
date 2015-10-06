a5a11f81dabd2451475069bea117c4ca5c32cee2
koenschepens <kschepens@gmail.com>
2015-10-06 02:36:22 +0200

    sadf

--- program/runner.py
+++ program/runner.py
@@ -1,11 +1,3 @@
 import os
-import xbmc
-import logging
-
-import xbmc
- 
-xbmc.executebuiltin('Notification(Hello World,This is a simple example of notifications,5000,/script.hellow.world.png)')
-
-addonFolder = "/home/osmc/.kodi/addons/service.keypad/" 
 
 os.system('sudo python ' + addonFolder + 'keypad.py')
