65a0b91833ba86ede5a28b13369f76e5e5c50853
koenschepens <kschepens@gmail.com>
2015-10-06 02:09:12 +0200

    asdf

--- program/runner.py
+++ program/runner.py
@@ -4,16 +4,4 @@ import logging
 
 addonFolder = "/home/osmc/.kodi/addons/service.keypad/" 
 
-config = ConfigParser.RawConfigParser()
-configFile = addonFolder + 'keypad.config'
-config.read(configFile)
-
-host = config.get("xbmc", "host")
-port = config.getint("xbmc", "port")
-
-xbmc = XBMCClient("OldPhone", "/etc/lirc/osmc-remote-lircd.png")
-xbmc.connect()
-
-xbmc.executebuiltin("Notification('Title','Message')")
-
-os.system('sudo python keypad.py')
+os.system('sudo python ' + addonFolder + 'keypad.py')
